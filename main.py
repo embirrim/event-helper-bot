3fc93e59ed63b7c30163e023bd1df9ca11d792ad
embirrim <benjamimbw@gmail.com>
2026-04-01 23:15:02 +0200

    Added all default channels, event IT role. Added check to see if event exists.

--- main.py
+++ main.py
@@ -33,27 +33,46 @@ GUILD_ID = discord.Object(id=471950858079436801)
 async def create_event(interaction: discord.Interaction, event_name: str):
      await interaction.response.defer(ephemeral = True)
      if discord.utils.get(interaction.guild.categories, name = event_name):
+          await interaction.followup.send("Event name already exists!")
           return
      
      try:
+          event_it_role = discord.utils.get(interaction.guild.roles, name = "Event IT")
           event_role = await interaction.guild.create_role(name = event_name)
           event_category = await interaction.guild.create_category(name = event_name)
           overwrites = {
                     interaction.guild.default_role: discord.PermissionOverwrite(view_channel = False, read_messages=False, connect=False),
-                    event_role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True)
+                    event_role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True),
+                    event_it_role: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=True, speak=True)
                     }
           await event_category.edit(overwrites = overwrites)
           event_text_channels = [
+               "announcements-and-call-times"
                "general",
                "network",
-               "test"
+               "offtopic"
           ]
           for channel in event_text_channels:
-               await interaction.guild.create_text_channel(name = channel, category = event_category)
+               channel = await interaction.guild.create_text_channel(name = channel, category = event_category)
           
-          await interaction.followup.send("Role, category, and channels created!")
+          event_voice_channels = [
+               "voice-and-remote-support",
+          ]
+          for channel in event_voice_channels:
+               channel = await interaction.guild.create_voice_channel(name = channel, category = event_category)
+
+          
+          event_forum_channels = [
+               "setup-forum",
+          ]
+          for channel in event_forum_channels:
+               await interaction.guild.create_forum(name = channel, category = event_category)
+
+
+          
+          await interaction.followup.send(f'Role, category, and channels created for event: {event_name}')
      except Exception as e:
-          await interaction.response.edit_message(f'An error occurred: {e}')
+          await interaction.followup.send(f'An error occurred: {e}')
 
      
 
